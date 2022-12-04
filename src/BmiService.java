772af8400ebbe32ca7cb2679b4526ae2c37e2c08
Kira <kira_pavlenko_13@mail.ru>
2022-12-04 18:12:06 +0300

    Second commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
     public double calculate(int weight, double height) {
-        return weight / height;
+        return weight /(height * height);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         int weight = 60;
-        double height = 1.72 * 1.72;
+        double height = 1.72;
         double index = service.calculate(weight, height);
         System.out.println(index);
     }
